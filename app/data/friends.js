97327504c3b3a6d6de5de5a87b4b1f7e354e29c2
syeda zaidi <zaidisyeda03@gmail.com>
2019-09-13 19:38:38 -0700

    added friends data  html code for home page

---
+++ app/data/friends.js
@@ -0,0 +1,242 @@
+[
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Seren",
+        "photo": "https://avatars1.githubusercontent.com/u/50847753?s=460&v=4",
+        "scores": [
+            "4",
+            "2",
+            "4",
+            "1",
+            "3",
+            "1",
+            "1",
+            "1",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Daniel",
+        "photo": "https://www.gannett-cdn.com/media/2019/08/05/USATODAY/usatsports/most-popular-dog-breeds-intro-image.jpg?width=2560",
+        "scores": [
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "3",
+            "3",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "name": "Elizabeth",
+        "photo": "facebook.com",
+        "scores": [
+            "2",
+            "1",
+            "2",
+            "1",
+            "3",
+            "3",
+            "2",
+            "1",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "Elizabeth",
+        "photo": "facebook.com",
+        "scores": [
+            "1",
+            "1",
+            "2",
+            "2",
+            "1",
+            "4",
+            "2",
+            "2",
+            "1",
+            "2"
+        ]
+    },
+    {
+        "name": "me",
+        "photo": "no",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Eddie",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Eddie",
+        "photo": "https://devilsworkshop.org/wp-content/uploads/sites/8/2013/01/small-facebook-profile-picture.jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3",
+            "4",
+            "3"
+        ]
+    },
+    {
+        "name": "Elizabeth",
+        "photo": "facebook.com",
+        "scores": [
+            "1",
+            "2",
+            "2",
+            "2",
+            "1",
+            "2",
+            "1",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "fdxv",
+        "photo": "fdfd.com",
+        "scores": [
+            "4",
+            "4",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "3",
+            "3",
+            "3"
+        ]
+    }
+]
