79a0665127473301ec1abbe3b36a0d21dd022df8
syeda zaidi <zaidisyeda03@gmail.com>
2019-10-04 18:35:14 -0700

    server setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+// get route to /survey to display survey page 
+// A default, catch-all route that leads to home.html which displays the home page
--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended : true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public")); // or
+// app.use(express.static(path.join(__dirname + "app/public")))
+
+// var routes = ("./app/routing");
+
+// app.use(routes);
+require("./app/routing/htmlRoutes.js")(app);
+require('./app/routing/apiRoutes.js')(app)
+
+app.listen(PORT, function () {
+    console.log ("server listeing on http://localhost:" + PORT );
+}).catch(function (err){ 
+    if (err) {
+        console.log(err);
+    }
+}) 
 
